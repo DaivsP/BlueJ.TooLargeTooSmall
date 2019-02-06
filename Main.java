2d85e0f82e68e1130d2ebcf251fe6c770c57084e
Davis Perchik <davisperchik@zipcoders-MacBook-Pro.local>
2019-02-05 20:30:55 -0500

    game works

--- Main.java
+++ Main.java
@@ -1,33 +1,38 @@
-
-/**
- * Write a description of class Main here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Main
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    import java.util.*;
+    public class Main {
+        
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Choose a number between 1 and 50");
+            
+            int r = (int)(Math.random()*50);
+            int high = 0;
+            int low = 0;
+            
+            while(true){
+                String userin = input.nextLine();
+                int guess = Integer.parseInt(userin);
+                
+                if(r == guess){
+                    System.out.println("You got it " + high + " tries high and " 
+                    + low + " low tries");
+                    break;
+                }
+                if(guess > r){
+                    System.out.println("Too high");
+                    high++;
+                }
+                else if(guess < r){
+                    System.out.println("Too low");
+                    low++;
+                }
+        }
     }
 }
+//set a range for how large or small the numbers can be
+//set a random number to a variable
+//make a user entry space, allow user to enter only numbers
+//keep track of how many guesses user submits
+//tell user too large for a big guess
+//tell user too small for a small guess
+//tell user if they guessed correctly and how many tries it took
